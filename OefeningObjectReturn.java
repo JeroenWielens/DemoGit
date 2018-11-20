d20cea618c20167916f9ecbc264ec85d7ba9aa09
JeroenWielens <jeroenwielens@gmail.com>
2018-11-20 09:40:16 +0100

    aanpassing 1

--- OefeningObjectReturn.java
+++ OefeningObjectReturn.java
@@ -3,6 +3,14 @@ package Voorbeeld2;
 public class OefeningObjectReturn {
     public static void main(String[] args) {
         System.out.println("Welkom");
+        ToegangsPoort tp = new ToegangsPoort();
+        tp.toelaten();
+    }
+}
+
+class ToegangsPoort {
+    void toelaten() {
+        System.out.println("Je bent toegelaten");
     }
 }
 
