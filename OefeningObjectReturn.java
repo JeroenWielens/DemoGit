ca82c6c650af0342994803402b1f24a02bbd56aa
JeroenWielens <jeroenwielens@gmail.com>
2018-11-20 10:26:20 +0100

    final aanpassing

--- OefeningObjectReturn.java
+++ OefeningObjectReturn.java
@@ -6,23 +6,41 @@ public class OefeningObjectReturn {
         ToegangsPoort tp = new ToegangsPoort();
         Persoon persoon = new Persoon();
         persoon.naam = "Henk";
+        persoon.pasje.geblokkeerd = true;
+        tp.toelaten(persoon);
+        Beveiliger Jan = new Beveiliger();
+        persoon.pasje = Jan.pasjeUitdelen(persoon);
         tp.toelaten(persoon);
     }
 }
 
 class ToegangsPoort {
     void toelaten(Persoon dePersoon) {
-        if (true){
-            System.out.println("Je bent toegelaten, welkom " + dePersoon.naam);
-        }else {
+        if (dePersoon.pasje.geblokkeerd){
             System.out.println("Je bent niet toegelaten");
+        }else {
+            System.out.println("Je bent toegelaten, welkom " + dePersoon.naam);
         }
     }
 }
 
 class Persoon {
     String naam;
-//    Pasje pasje;
+    Pasje pasje = new Pasje();
 }
 
+class Pasje {
+    boolean geblokkeerd = false;
+}
 
+class Beveiliger {
+    Pasje pasjeUitdelen(Persoon persoon) {
+        if (persoon.pasje.geblokkeerd) {
+            System.out.println("Even de gegevens checken");
+            return new Pasje();
+        }else{
+            System.out.println("Val me niet lastig");
+            return persoon.pasje;
+        }
+    }
+}
