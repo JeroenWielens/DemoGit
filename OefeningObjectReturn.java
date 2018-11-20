a8d314ed5f387bdd6171e12758a49b2c3b1039ff
JeroenWielens <jeroenwielens@gmail.com>
2018-11-20 10:12:33 +0100

    Aanpassing 3

--- OefeningObjectReturn.java
+++ OefeningObjectReturn.java
@@ -4,18 +4,25 @@ public class OefeningObjectReturn {
     public static void main(String[] args) {
         System.out.println("Welkom");
         ToegangsPoort tp = new ToegangsPoort();
-        tp.toelaten();
+        Persoon persoon = new Persoon();
+        persoon.naam = "Henk";
+        tp.toelaten(persoon);
     }
 }
 
 class ToegangsPoort {
-    void toelaten() {
+    void toelaten(Persoon dePersoon) {
         if (true){
-            System.out.println("Je bent toegelaten");
+            System.out.println("Je bent toegelaten, welkom " + dePersoon.naam);
         }else {
             System.out.println("Je bent niet toegelaten");
         }
     }
+}
+
+class Persoon {
+    String naam;
+//    Pasje pasje;
 }
 
 
